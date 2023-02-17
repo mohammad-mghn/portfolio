f56fa60b108bfb2f42e9ae9c9765838fd99cb19f
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-02-17 20:42:14 +0330

    initial redesign

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="fa">
+      <head />
+
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+async function Page() {
+  return <main className="pt-24">s</main>;
+}
+
+export default Page;
