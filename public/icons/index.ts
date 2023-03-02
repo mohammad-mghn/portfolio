7787a35b4b5e2cf44987ce1c2869cf6be8f3df41
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-03-02 23:52:51 +0330

    add scroll to top

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -1,3 +1,4 @@
+export { default as TopIcon } from "./top.svg";
 export { default as MenuIcon } from "./menu.svg";
 export { default as FarsiIcon } from "./farsi.svg";
 export { default as CloseIcon } from "./close.svg";
