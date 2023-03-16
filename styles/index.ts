d1f7e905a41466ddfd4161f8cb42b0587acc3e2e
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-03-16 16:45:03 +0330

    fix button animation bug

--- styles/index.ts
+++ styles/index.ts
@@ -4,7 +4,7 @@ export const styles = {
   h3: "",
   box: "rounded-lg shadow-dark bg-primary",
   button:
-    "flex items-center gap-x-3 rounded-lg border-2 md:border-[3px] text-sm md:text-[1.125rem] text-brand font-semibold border-brand group hover:bg-brand hover:text-primary transition-all duration-300 ease-in-out",
+    "flex items-center gap-x-3 rounded-lg border-2 md:border-[3px] text-sm md:text-[1.125rem] text-brand font-semibold border-brand group hover:bg-brand hover:text-primary transition-all duration-300 ease-in-out button",
   divider: "my-6 bg-border rounded-lg w-full h-1",
   defautTransition: "transition-all duration-300",
 };
