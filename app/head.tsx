62f4d6f668ab204f914a81295a72a91a0ed462b2
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-03-21 15:23:21 +0330

    add meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -5,6 +5,31 @@ export default function Head() {
       <link rel="icon" href="/v-icon.svg" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="vito mohagheghian portfolio, web developer(Front-end)" />
+      <meta
+        name="keywords"
+        content="ویتو محققیان , محمد محققیان , vito moahgehghian , mohammad mohagheghian , برنامه نویس ReactJs, برنامه نویس Front-end, برنامه نویس وب "
+      />
+      <link rel="canonical" href="https://vito.vercel.app" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Vito Mohagheghian" />
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        href="https://vito.vercel.app/about"
+        title="About Vito Mohagheghian درباره ی ویتو محققیان"
+      />
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        href="https://vito.vercel.app/projects"
+        title="Vito Mohagheghian Projects پروژه های ویتو محققیان"
+      />
+      <link
+        rel="alternate"
+        type="application/rss+xml"
+        href="https://vito.vercel.app/contact"
+        title="keep in touch with Vito Mohagheghian ارتباط با ویتو محققیان"
+      />
     </>
   );
 }
