cc3892d426b04ca3380e04be6c32b2e9dbc59f5a
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-04-13 15:18:39 +0330

    fix meta tags

--- app/fa/head.tsx
+++ app/fa/head.tsx
@@ -4,10 +4,12 @@ export default function Head() {
       <title>ویتو محققیان</title>
       <link rel="icon" href="../v-icon.svg" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="وبسایت محمد محققیان ملقب به ویتو محققیان ، برنامه نویس وب -فرانت اند" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="vito mohagheghian" />
       <meta
-        name="description"
-        content="وبسایت محمد محققیان ملقب به ویتو محققیان ، برنامه نویس وب -فرانت اند"
-        key="desc"
+        property="og:image"
+        content="https://github.com/vito-mohagheghian/portfolio/blob/images-db/portfolio/1.png?raw=true"
       />
     </>
   );
--- app/projects/head.tsx
+++ app/projects/head.tsx
@@ -4,7 +4,17 @@ export default function Head() {
       <title>Vito Mohagheghian • About</title>
       <link rel="icon" href="/v-icon.svg" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="vito mohagheghian portfolio and blog" />
+      <meta name="description" content="vito mohagheghian portfolio, web developer(Front-end) projects" />
+      <meta
+        name="keywords"
+        content="vito mohagheghian projects, mohammad mohagheghian projects, پروژه ها ویتو محققیان, پروژه ها محمد محققیان, Front-end developer, vito projects"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="vito mohagheghian" />
+      <meta
+        property="og:image"
+        content="https://github.com/vito-mohagheghian/portfolio/blob/images-db/portfolio/1.png?raw=true"
+      />
     </>
   );
 }
