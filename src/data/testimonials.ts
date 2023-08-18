8726963c7336fbb128c3683c2acdfda155bea6c6
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-08-18 16:40:21 +0330

    add new testimonial template

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -8,6 +8,12 @@ export const testimonials: testimonialType[] = [
     text: "Vito is a bright, young man who has a deep passion for programming and his work. Being at a young age, he possesses abundant energy and enthusiasm towards his tasks, constantly seeking opportunities for learning and skill enhancement. His capabilities are evident from the projects he has undertaken and completed thus far.",
   },
   {
+    name: "Mahdi Nazari - Front-end Dev, PO",
+    link: "https://mahdi-nazari.ir/",
+    socialMedia: "Portfolio",
+    text: "lorem Eu ex officia sit id mollit sit ex aliquip duis esse ullamco. Lorem eiusmod deserunt ea duis labore qui veniam laborum. Deserunt quis aliquip culpa est. Nulla esse Lorem amet laborum dolor velit exercitation. Ea esse duis non consectetur dolore Lorem non id reprehenderit id dolor sit excepteur.",
+  },
+  {
     name: "Dayana Raadfar - Designer",
     link: "https://www.linkedin.com/in/dayanaraadfar",
     socialMedia: "linkedIn",
