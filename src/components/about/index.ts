ec818900d7a4bbc7f09ed2b826eeb9c315ea2dcc
vito mohagheghian <vito.mohagheghian@gmail.com>
2023-08-24 23:18:13 +0330

    clean code, improve folder structure, fix import mistakes, add image server side rendering, fix some UI rtl bugs

--- src/components/about/index.ts
+++ src/components/about/index.ts
@@ -2,6 +2,6 @@ import Skills from "./skills";
 import Languages from "./languages";
 import AboutLanding from "./landing";
 import Testimonials from "./testimonials";
-import AutoBiography from "./autobiography";
+import AutoBiography from "./autobiography/index";
 
 export { Skills, Languages, AboutLanding, Testimonials, AutoBiography };
